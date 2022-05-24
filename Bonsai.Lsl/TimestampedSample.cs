d138cded9ada219d495013b5a316815b44fda730
RoboDoig <erskine94@gmail.com>
2022-05-24 13:23:45 +0100

    Example broadcaster / receiver

---
+++ Bonsai.Lsl/TimestampedSample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.LSL
+{
+    public class TimestampedSample<T>
+    {
+        public double SampleTime;
+        public T[] SampleArray;
+
+        public TimestampedSample(double sampleTime, T[] sampleArray)
+        {
+            SampleTime = sampleTime;
+            SampleArray = sampleArray;
+        }
+    }
+}
