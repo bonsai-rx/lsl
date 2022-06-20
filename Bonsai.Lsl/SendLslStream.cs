561d26c1472449a2c04e2d752fe0c94f0151adbc
RoboDoig <erskine94@gmail.com>
2022-06-20 11:49:00 +0100

    sketching out osc style structure

---
+++ Bonsai.Lsl/SendLslStream.cs
@@ -0,0 +1,43 @@
+﻿using Bonsai.Expressions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+
+
+namespace Bonsai.Lsl
+{
+    [WorkflowElementCategory(ElementCategory.Sink)]
+    public class SendLslStream : SingleArgumentExpressionBuilder
+    {
+        public string StreamName { get; set; }
+        public string StreamType { get; set; }
+        public string Uid { get; set; }
+
+        public override Expression Build(IEnumerable<Expression> arguments)
+        {
+            var streamName = StreamName;
+            var source = arguments.First();
+            var parameterTypes = source.Type.GetGenericArguments();
+            var inputParameter = Expression.Parameter(parameterTypes[0]);
+            var builder = Expression.Constant(this);
+
+            var buildStream = StreamBuilder.Stream(streamName, inputParameter);
+
+            // this.Process(types, source, streambuilder)
+            return Expression.Call(builder, null); // Placeholder
+        }
+
+        IObservable<int> Process(IObservable<int> source)
+        {
+            return Observable.Return(1);
+        }
+
+        IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return Observable.Never<TSource>(); // Placeholder
+        }
+    }
+}
---
+++ Bonsai.Lsl/StreamBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Bonsai.Lsl
+{
+    static class StreamBuilder
+    {
+        public static Expression Stream(string streamName, Expression parameter)
+        {
+
+        }
+
+        static Expression CreateStreamBuilder(Expression parameter, StringBuilder typeTagBuilder)
+        {
+            var type = parameter.Type;
+            var typeCode = Type.GetTypeCode(type);
+            switch (typeCode)
+            {
+                // float 
+                case TypeCode.Single:
+                    return null;
+
+                // double
+                case TypeCode.Double:
+                    return null;
+
+                // int
+                case TypeCode.Int32:
+                    return null;
+
+                // short
+                case TypeCode.Int16:
+                    return null;
+
+                // char
+                case TypeCode.Char:
+                    return null;
+#
+                // string
+                case TypeCode.String:
+                    return null;
+
+                //object
+                case TypeCode.Object:
+                    return null;
+            }
+        }
+    }
+}
