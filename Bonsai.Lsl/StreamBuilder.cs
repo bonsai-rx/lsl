877788bc019d3362f8f8355a589e1afdbe926fcc
RoboDoig <erskine94@gmail.com>
2022-06-22 15:29:36 +0100

    channel count expression for stream creation

--- Bonsai.Lsl/StreamBuilder.cs
+++ Bonsai.Lsl/StreamBuilder.cs
@@ -11,16 +11,15 @@ namespace Bonsai.Lsl
     static class StreamBuilder
     {
 
-        public static StreamOutlet CreateOutlet(string streamName, string streamType, channel_format_t channelFormat)
+        public static StreamOutlet CreateOutlet(string streamName, string streamType, int channelCount, channel_format_t channelFormat)
         {
-            var info = new StreamInfo(streamName, streamType, 1, LSL.IRREGULAR_RATE, channelFormat, "");
+            var info = new StreamInfo(streamName, streamType, channelCount, LSL.IRREGULAR_RATE, channelFormat, "");
             return new StreamOutlet(info);
         }
 
         static readonly MethodInfo CreateOutletMethod = typeof(StreamBuilder).GetMethod(nameof(StreamBuilder.CreateOutlet));
 
         static readonly MethodInfo WriteFloat = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(float[]), typeof(double), typeof(bool) });
-        static readonly MethodInfo WriteInt64 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(float[]), typeof(double), typeof(bool) }); // no push_sample for long so we will convert to float[]
         static readonly MethodInfo WriteDouble = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(double[]), typeof(double), typeof(bool) });
         static readonly MethodInfo WriteInt32 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(int[]), typeof(double), typeof(bool) });
         static readonly MethodInfo WriteInt16 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(short[]), typeof(double), typeof(bool) });
@@ -32,36 +31,38 @@ namespace Bonsai.Lsl
             var type = parameter.Type;
             var typeCode = Type.GetTypeCode(type);
 
+            Expression channelCount = type.IsArray ? Expression.ArrayLength(parameter) : Expression.Constant(1);
+
             switch (typeCode)
             {
                 // float
                 case TypeCode.Single:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_float32, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_float32, typeof(channel_format_t)));
 
                 // double
                 case TypeCode.Double:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_double64, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_double64, typeof(channel_format_t)));
 
                 // int
                 case TypeCode.Int32:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_int32, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_int32, typeof(channel_format_t)));
 
                 // short
                 case TypeCode.Int16:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_int16, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_int16, typeof(channel_format_t)));
 
                 // string
                 case TypeCode.String:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_string, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_string, typeof(channel_format_t)));
 
                 // long
                 case TypeCode.Int64:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_int64, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_int64, typeof(channel_format_t)));
 
                 // For any other types, we need largest type that can hold other types (double64)
                 case TypeCode.Object:
                 default:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_double64, typeof(channel_format_t)));
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, channelCount, Expression.Constant(channel_format_t.cf_double64, typeof(channel_format_t)));
             }
         }
 
@@ -69,7 +70,6 @@ namespace Bonsai.Lsl
         {
             var type = data.Type;
             var typeCode = Type.GetTypeCode(type);
-            NewArrayExpression dataArray;
 
             switch (typeCode)
             {
@@ -100,9 +100,9 @@ namespace Bonsai.Lsl
 
                 // long
                 case TypeCode.Int64:
-                    data = Expression.Convert(data, typeof(float));
-                    dataArray = Expression.NewArrayInit(typeof(float), new List<Expression> { data });
-                    return Expression.Call(outlet, WriteInt64, dataArray, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
+                    data = Expression.Convert(data, typeof(float)); // no LSL long writer, need to convert to float
+                    data = Expression.NewArrayInit(typeof(float), new List<Expression> { data });
+                    return Expression.Call(outlet, WriteFloat, data, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
 
                 case TypeCode.Object:
                 default:
