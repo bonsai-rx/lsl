0bb6119db2423ab3d38b2fab89f64af7bed4fe1a
RoboDoig <erskine94@gmail.com>
2022-06-22 14:09:51 +0100

    more data builders

--- Bonsai.Lsl/StreamBuilder.cs
+++ Bonsai.Lsl/StreamBuilder.cs
@@ -19,7 +19,8 @@ namespace Bonsai.Lsl
 
         static readonly MethodInfo CreateOutletMethod = typeof(StreamBuilder).GetMethod(nameof(StreamBuilder.CreateOutlet));
 
-        static readonly MethodInfo WriteInt64 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(float[]), typeof(double), typeof(bool) });
+        static readonly MethodInfo WriteFloat = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(float[]), typeof(double), typeof(bool) });
+        static readonly MethodInfo WriteInt64 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(float[]), typeof(double), typeof(bool) }); // no push_sample for long so we will convert to float[]
         static readonly MethodInfo WriteDouble = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(double[]), typeof(double), typeof(bool) });
         static readonly MethodInfo WriteInt32 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(int[]), typeof(double), typeof(bool) });
         static readonly MethodInfo WriteInt16 = typeof(StreamOutlet).GetMethod(nameof(StreamOutlet.push_sample), new[] { typeof(short[]), typeof(double), typeof(bool) });
@@ -33,20 +34,34 @@ namespace Bonsai.Lsl
 
             switch (typeCode)
             {
+                // float
+                case TypeCode.Single:
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_float32, typeof(channel_format_t)));
+
+                // double
+                case TypeCode.Double:
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_double64, typeof(channel_format_t)));
+
                 // int
                 case TypeCode.Int32:
                     return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_int32, typeof(channel_format_t)));
 
-                // float
-                case TypeCode.Single:
-                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_float32, typeof(channel_format_t)));  
+                // short
+                case TypeCode.Int16:
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_int16, typeof(channel_format_t)));
+
+                // string
+                case TypeCode.String:
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_string, typeof(channel_format_t)));
 
                 // long
                 case TypeCode.Int64:
                     return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_int64, typeof(channel_format_t)));
 
+                // For any other types, we need largest type that can hold other types (double64)
+                case TypeCode.Object:
                 default:
-                    return null;
+                    return Expression.Call(CreateOutletMethod, nameParam, typeParam, Expression.Constant(channel_format_t.cf_double64, typeof(channel_format_t)));
             }
         }
 
@@ -58,14 +73,30 @@ namespace Bonsai.Lsl
 
             switch (typeCode)
             {
+                // float
+                case TypeCode.Single:
+                    data = Expression.NewArrayInit(typeof(float), new List<Expression> { data });
+                    return Expression.Call(outlet, WriteFloat, data, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
+
+                // double
+                case TypeCode.Double:
+                    data = Expression.NewArrayInit(typeof(double), new List<Expression> { data });
+                    return Expression.Call(outlet, WriteDouble, data, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
+
                 // int
                 case TypeCode.Int32:
                     data = Expression.NewArrayInit(typeof(int), new List<Expression> { data });
                     return Expression.Call(outlet, WriteInt32, data, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
 
-                // float
-                case TypeCode.Single:
-                    return null;
+                // short
+                case TypeCode.Int16:
+                    data = Expression.NewArrayInit(typeof(short), new List<Expression> { data });
+                    return Expression.Call(outlet, WriteInt16, data, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
+
+                // string
+                case TypeCode.String:
+                    data = Expression.NewArrayInit(typeof(string), new List<Expression> { data });
+                    return Expression.Call(outlet, WriteString, data, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
 
                 // long
                 case TypeCode.Int64:
@@ -73,6 +104,7 @@ namespace Bonsai.Lsl
                     dataArray = Expression.NewArrayInit(typeof(float), new List<Expression> { data });
                     return Expression.Call(outlet, WriteInt64, dataArray, Expression.Constant(0.0, typeof(double)), Expression.Constant(true, typeof(bool)));
 
+                case TypeCode.Object:
                 default:
                     return null;
             }
